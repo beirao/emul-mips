7c1b0d71dbd8a0ab97db654298ef8e25f889c105
RomaneMoulin <56441758+RomaneMoulin@users.noreply.github.com>
2020-11-28 21:08:31 +0100

    Petites modifs (suppression du proto de testChaine qui n'est pas définie dans traitement_instructions.h)

--- header/traitement_instructions.h
+++ header/traitement_instructions.h
@@ -10,14 +10,11 @@
 #define END 999 /*correspond a la fin dans un tableau de int dans la fonction chaine*/
 
 
-void lireDonnees(char fichier_commande[], char fichier_hexa[]);
-
 FILE *ouvertureFichier(char *chemin_fichier, char *mode);
+void lireDonnees(char fichier_commande[], char fichier_hexa[]);
 char *traitementChaine(char *chaine);
-int testChaine(char chaine[], char mot[]);
-void pushHexa(int hexa, FILE *fichier_arrive, int sw_lw);
 void argumentToTab(char *chaine, int *argument);
-int testChaine(char chaine[], char mot[]);
+void pushHexa(int hexa, FILE *fichier_arrive, int sw_lw);
 
 
 #endif
